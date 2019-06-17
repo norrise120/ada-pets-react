32cd7947922e8eef6c7dd00a3006c72f0b6ed441
Erica Norris <norrise120@gmail.com>
2019-06-17 10:44:59 -0700

    updated petlist to make petcards of all of the pets

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -7,9 +7,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const PetList = (props) => {
 
+  const petCardComponents = props.pets.map((pet) => {
+    const { id, name, species, about, location } = pet;
+    return (<li key={id}><PetCard
+      key={id}
+      name={name}
+      species={species}
+      about={about}
+      location={location}
+    />
+    </li>);
+  });
 
   return (
     <div className="card-group">
+      {petCardComponents}
     </div>
   )
 }
